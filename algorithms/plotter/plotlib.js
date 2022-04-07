49aa69727545a4e8288d4e785cc86d84aa9f6a67
josipbusic431 <47043034+josipbusic431@users.noreply.github.com>
2022-04-07 19:31:36 +0200

    nn

--- algorithms/plotter/plotlib.js
+++ algorithms/plotter/plotlib.js
@@ -1,32 +1,63 @@
-function Plotter(id){
+function Plotter(id, plot_type){
     this.canvas = document.getElementById(id);
     this.ctx = this.canvas.getContext("2d");
-    this.xMin = 0;
-    this.yMin = 0;
-    this.xMax = this.canvas.width;
-    this.yMax = this.canvas.height;
+    const _plotType = plot_type;
 
-    // Plot Line Function
-    this.plotLine = function(x0, y0, x, y, color) {
-        this.ctx.moveTo(x0, y0);
-        this.ctx.lineTo(x, y);
-        this.ctx.strokeStyle = color;
-        this.ctx.stroke();
-    };
+    this.show = function(labels, real_data, real_data_label, pred_data, pred_data_label){
+        let dataColor1 = utils.getColor(0);
+        let data1 = new Dataset(real_data_label, real_data, dataColor1, dataColor1);
 
-    // Transform XY Function
-    this.transformXY = function() {
-        this.ctx.transform(1, 0, 0, -1, 0, this.canvas.height)
-    };
+        let dataColor2 = utils.getColor(1);
+        let data2 = new Dataset(pred_data_label, pred_data, dataColor2, dataColor2);
 
-    // Pot Points Function
-    this.plotPoints = function(n, xArr, yArr, color, radius = 3) {
-        for (let i = 0; i < n; i++) {
-            this.ctx.fillStyle = color;
-            this.ctx.beginPath();
-            this.ctx.ellipse(xArr[i], yArr[i], radius, radius, 0, 0, Math.PI * 2);
-            this.ctx.fill();
+        var config = new MyChartConfig(_plotType, labels, [data1.json(), data2.json()]);
+
+        if (window.myChart != null) {
+            try {
+                window.myChart.destroy();
+            } catch (error) { }
         }
-    };
+        window.myChart = new Chart(this.ctx, config.config());
+        window.myChart.render();
+    };    
+};
 
+function MyChartConfig(type, labels, datasets){
+    let _type = type;
+    let _labels = labels;
+    let _datasets = datasets;
+
+    this.data = function(){
+        return {
+            labels: _labels,
+            datasets: _datasets
+        };
+    };
+    this.config = function(){
+         return {
+            type: _type,
+            data: this.data()
+         };
+    };
+    Object.defineProperty(this, "labels",{
+        get: function(){ return labels; },
+        set: function(vaule){ labels = value; }
+    });
+};
+
+function Dataset(label, data, borderColor, backgroundColor){
+    const _label = label;
+    const _data = data;
+    const _borderColor = borderColor;
+    const _backgroundColor = backgroundColor;
+
+    this.json = function(){
+        return {
+            label: _label,
+            data: _data,
+            borderColor: _borderColor,
+            backgroundColor: _backgroundColor,
+            tension: 0.4
+        };
+    };
 };
